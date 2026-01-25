56d2d1f1a29a661f2e85bd9fc3c0a22b80f743e6
pradeep misal <144873499+pradeepmisal@users.noreply.github.com>
2026-01-25 16:51:02 +0530

    Time: 44 ms (6.51%), Space: 48 MB (28.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int lt =0;
-        
-        //pushing non zero forward
-        for(int i =0; i < n; i++){
-            if(nums[i] != 0){
-                nums[lt] = nums[i];// copies the value of array 
-                lt++;
+        for(int i = 0; i < n; i++){
+            if(nums [i] == 0){
+                for(int j = i + 1; j < n ; j++ ){
+                    if(nums[j] !=  0){
+                        // swap
+                        int temp = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = temp;
+                        break;
+                    }
+                }
             }
         }
-        //now just move zeros to end
-        for(int i=lt; i<n; i++ ){
-            
-                nums[i] = 0; 
-            
         }
-    }
 }
+    

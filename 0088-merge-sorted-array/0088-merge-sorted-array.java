ac1fcc5ab69f51ac6681943580c16ce32c24dcd9
pradeep misal <144873499+pradeepmisal@users.noreply.github.com>
2026-02-04 10:14:55 +0530

    Time: 1 ms (24.2%), Space: 43.5 MB (93.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int midx = m - 1;
-        int nidx = n - 1;
-        int right = m + n - 1;
-
-        while (nidx >= 0) {
-            if (midx >= 0 && nums1[midx] > nums2[nidx]) {
-                nums1[right] = nums1[midx];
-                midx--;
-            } else {
-                nums1[right] = nums2[nidx];
-                nidx--;
-            }
-            right--;
-        }        
+        ArrayList<Integer> set = new ArrayList<>();
+       
+        for(int i = 0; i < m ; i++){
+            set.add(nums1[i]);
+        }
+        for(int j=0; j < n; j++){
+            set.add(nums2[j]);
+        }
+       
+        Collections.sort(set);
+        //basically here only copying nubers in nums1
+        for(int i = 0; i < set.size(); i++){
+            nums1[i] = set.get(i);
+        }
     }
 }
